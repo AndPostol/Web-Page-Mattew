3f16f774b828d6db78d1c99f8b0ebf629d7b371f
Andres Apostol <aandresaapostol@gmail.com>
2024-08-02 20:04:08 -0400

    add final details

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-import photo from './assets/photo-perfil.jpg'
+import photo from './assets/perfil-02.jpeg'
 // Icons
 import pdfIcon from './assets/pdfIcon.svg'
 import excelIcon from "./assets/excel.svg";
@@ -7,7 +7,8 @@ import pythonIcon from "./assets/python.svg";
 import sqlIcon from "./assets/sql.svg";
 import tableulIcon from "./assets/tableau.svg";
 import tableuWhite from "./assets/tableauIcon.svg"
-
+import pdf from "./assets/Profile.pdf";
+import linkedInIcon from "./assets/linkedin.svg";
 
 import './App.css'
 import CardButton from './components/CardButton'
@@ -20,22 +21,21 @@ function App() {
       <div className='w-full pb-12 md:w-2/3 h-2/4 flex sm:flex-col md:flex-row justify-center  items-center'>
         <div className='text-white md:block flex flex-col items-center'>
           <h1 className='font-["Raleway"] pt-10 text-center md:text-start font-bold text-6xl'>Matthew Tabola</h1>
-        <img src={photo} alt="Matt" className='w-2/3 py-6 h-full rounded-full md:hidden '/>
+        <img src={photo} alt="Matt" className='w-2/3 p-4 md:py-6 h-full rounded-full md:hidden '/>
           <h2 className='text-xl w-3/4 font-semibold'>Shift Assistant at Amazon / <br /> Data Analytics Fellow</h2>
           <h3 className='py-10 w-3/4'>
           Hello! I am formerly an <span className='font-bold'>Assistant Manager</span> at Amazon, also enrolled in a <span className='font-bold'> Data Analytics Program</span>. 
           <br />
           <br />
-          You'll find me to be extremely dependable as well as versatile, eager to adapt to any new situation or challenge that arises. I also love amateur radio, and you can often catch me on the air!
+          {"You'll"} find me to be extremely dependable as well as versatile, eager to adapt to any new situation or challenge that arises. I also love amateur radio, and you can often catch me on the air!
           </h3>
-          <div className='w-3/4 flex flex-wrap  gap-5 justify-center lg:flex-nowrap lg:justify-start'>
+          <div className='w-3/4 flex flex-wrap  gap-5 justify-between'>
           <ActionButton icon={tableuWhite} url="https://public.tableau.com/app/profile/matthew.tabola/vizzes" text="Tableau" className="bg-[#3F8E00] border border-[#62BA1B] drop-shadow-lg"/>
-          <ActionButton icon={pdfIcon} url="https://public.tableau.com/app/profile/matthew.tabola/vizzes" text="Resume" className="bg-[#3F8E00] border border-[#62BA1B] drop-shadow-lg"/>
-          
+          <ActionButton icon={pdfIcon} url={pdf} text="Resume" className="bg-[#3F8E00] border border-[#62BA1B] drop-shadow-lg"/>
+          <ActionButton icon={linkedInIcon} url="https://www.linkedin.com/in/matthew-tabola-844141190/" text="LinkedIn" classImg="mb-[9px]" className="bg-[#0e76a8] border grow border-[#0e76a8] drop-shadow-lg justify-center"/>
           </div>
-
         </div>
-        <img src={photo} alt="Matt" className='w-2/5 h-full rounded-full hidden md:block'/>
+        <img src="https://media.licdn.com/dms/image/D4D03AQEF62FmRtnnPw/profile-displayphoto-shrink_800_800/0/1719950910219?e=1727913600&v=beta&t=ozZABO3vDRhoPeGQOLK3kv9laGbL_JVmEpiDxb1LldQ" alt="Matt" className='w-2/5 h-full rounded-full hidden md:block'/>
       </div>
       <div className='w-3/4 mb-10'>
       <h4 className='text-white font-["IBM Plex Mono"] text-lg pb-5'>I Work with</h4>
--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types";
-function ActionButton({icon, text, url, className}){
+function ActionButton({icon, text, url, classImg ,className}){
     return (
-        <a href={url} target="_blank" className={`flex w-full lg:w-[160px] min-w-[200px] h-[62px] ${className} `}>
-              <img src={icon} alt="Icon PDF" className='w-[30px] mx-4'/>
+        <a href={url} target="_blank" className={`flex max-[1400px]:w-full  min-[1401px]:w-[160px] min-w-[200px] h-[62px] ${className} `}>
+              <img src={icon} alt="Icon PDF" className={`w-[30px] mx-4 ${classImg}`}/>
               <span className='my-auto'>{text}</span>
         </a>
     )
@@ -12,5 +12,6 @@ ActionButton.propTypes = {
     text : PropTypes.string.isRequired,
     url : PropTypes.string.isRequired,
     className : PropTypes.string.isRequired,
+    classImg : PropTypes.string
 }
 export default ActionButton;
